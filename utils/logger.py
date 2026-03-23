b32689a718540eb519cfc189ed4ec6e6411144f5
murodov109 <akabekov77@gmail.com>
2026-03-24 01:33:52 +0500

    Create utils/logger.py with logging configuration

---
+++ utils/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+# Logging configuration
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
